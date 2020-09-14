3f294f2e9af96eb493cb8f3e6a19d2bcb5f22623
Matthew Herman <mattherman11@gmail.com>
2020-09-14 18:17:12 -0500

    Some scanning

--- Lox/Program.cs
+++ Lox/Program.cs
@@ -3,14 +3,16 @@ using System.IO;
 
 namespace Lox
 {
-    public class Program
+    public class Lox
     {
-        public static int Main(string[] args)
+        private static bool _hadError = false;
+
+        public static void Main(string[] args)
         {
             if (args.Length > 1)
             {
                 Console.WriteLine("Usage: Lox [script]");
-                return 64;
+                Environment.Exit(64);
             }
             else if (args.Length == 1)
             {
@@ -21,13 +23,15 @@ namespace Lox
                 RunPrompt();
             }
 
-            return 0;
+            Environment.Exit(0);
         }
 
         private static void RunFile(string filename)
         {
             var source = File.ReadAllText(filename);
             Run(source);
+            if (_hadError)
+                Environment.Exit(65);
         }
 
         private static void RunPrompt()
@@ -38,6 +42,7 @@ namespace Lox
                 var input = Console.ReadLine();
                 if (input == null) break;
                 Run(input);
+                _hadError = false;
             }
         }
 
@@ -48,6 +53,17 @@ namespace Lox
             {
                 Console.WriteLine(token);
             }
+        }
+
+        internal static void Error(int line, string message)
+        {
+            Report(line, string.Empty, message);
+        }
+
+        private static void Report(int line, string where, string message)
+        {
+            Console.WriteLine($"[line {line}] Error{where}: {message}");
+            _hadError = true;
         }
     }
 }
--- Lox/Scanner.cs
+++ Lox/Scanner.cs
@@ -1,17 +1,70 @@
+using System.Collections.Generic;
+
 namespace Lox
 {
     public class Scanner
     {
         private readonly string _source;
+        private readonly IList<Token> _tokens = new List<Token>();
+
+        private bool IsAtEnd() => current >= _source.Length;
+
+        private int start = 0;
+        private int current = 0;
+        private int line = 1;
 
         public Scanner(string source)
         {
             _source = source;
         }
 
-        public string[] ScanTokens()
+        public IList<Token> ScanTokens()
         {
-            return _source.Split(' ');
+            while(!IsAtEnd())
+            {
+                start = current;
+                ScanToken();
+            }
+
+            _tokens.Add(new Token(TokenType.EOF, "", null, line));
+            return _tokens;
+        }
+
+        private void ScanToken()
+        {
+            switch(Advance())
+            {
+                case '(': AddToken(TokenType.LEFT_PAREN); break;
+                case ')': AddToken(TokenType.RIGHT_PAREN); break;
+                case '{': AddToken(TokenType.LEFT_BRACE); break;
+                case '}': AddToken(TokenType.RIGHT_BRACE); break;
+                case ',': AddToken(TokenType.COMMA); break;
+                case '.': AddToken(TokenType.DOT); break;
+                case '-': AddToken(TokenType.MINUS); break;
+                case '+': AddToken(TokenType.PLUS); break;
+                case ';': AddToken(TokenType.SEMICOLON); break;
+                case '*': AddToken(TokenType.STAR); break;
+                default:
+                    Lox.Error(line, "Unexpected character.");
+                    break;
+            }
+        }
+
+        private char Advance()
+        {
+            current++;
+            return _source[current - 1];
+        }
+
+        private void AddToken(TokenType type)
+        {
+            AddToken(type, null);
+        }
+
+        private void AddToken(TokenType type, object literal)
+        {
+            var text = _source.Substring(start, current);
+            _tokens.Add(new Token(type, text, literal, line));
         }
     }
 }
