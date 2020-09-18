cd32e29640fda8e0bebc174287ef1ea33985d6be
Matthew Herman <mattherman11@gmail.com>
2020-09-17 22:11:28 -0500

    Fixed scanning out of bounds and added operators

--- Lox/Scanner.cs
+++ Lox/Scanner.cs
@@ -34,20 +34,69 @@ namespace Lox
         {
             switch (Advance())
             {
-                case '(': AddToken(TokenType.LEFT_PAREN); break;
-                case ')': AddToken(TokenType.RIGHT_PAREN); break;
-                case '{': AddToken(TokenType.LEFT_BRACE); break;
-                case '}': AddToken(TokenType.RIGHT_BRACE); break;
-                case ',': AddToken(TokenType.COMMA); break;
-                case '.': AddToken(TokenType.DOT); break;
-                case '-': AddToken(TokenType.MINUS); break;
-                case '+': AddToken(TokenType.PLUS); break;
-                case ';': AddToken(TokenType.SEMICOLON); break;
-                case '*': AddToken(TokenType.STAR); break;
+                case '(':
+                    AddToken(TokenType.LEFT_PAREN);
+                    break;
+                case ')':
+                    AddToken(TokenType.RIGHT_PAREN);
+                    break;
+                case '{':
+                    AddToken(TokenType.LEFT_BRACE);
+                    break;
+                case '}':
+                    AddToken(TokenType.RIGHT_BRACE);
+                    break;
+                case ',':
+                    AddToken(TokenType.COMMA);
+                    break;
+                case '.':
+                    AddToken(TokenType.DOT);
+                    break;
+                case '-':
+                    AddToken(TokenType.MINUS);
+                    break;
+                case '+':
+                    AddToken(TokenType.PLUS);
+                    break;
+                case ';':
+                    AddToken(TokenType.SEMICOLON);
+                    break;
+                case '*':
+                    AddToken(TokenType.STAR);
+                    break;
+                case '!':
+                    AddToken(Match('=') ?
+                        TokenType.BANG_EQUAL :
+                        TokenType.BANG);
+                    break;
+                case '=':
+                    AddToken(Match('=') ?
+                        TokenType.EQUAL_EQUAL :
+                        TokenType.EQUAL);
+                    break;
+                case '<':
+                    AddToken(Match('=') ?
+                        TokenType.LESS_EQUAL :
+                        TokenType.LESS);
+                    break;
+                case '>':
+                    AddToken(Match('=') ?
+                        TokenType.GREATER_EQUAL :
+                        TokenType.GREATER);
+                    break;
                 default:
                     Lox.Error(line, "Unexpected character.");
                     break;
             }
+        }
+
+        private bool Match(char expected)
+        {
+            if (IsAtEnd()) return false;
+            if (_source[current] != expected) return false;
+
+            current++;
+            return true;
         }
 
         private char Advance()
@@ -63,7 +112,7 @@ namespace Lox
 
         private void AddToken(TokenType type, object literal)
         {
-            var text = _source.Substring(start, current);
+            var text = _source.Substring(start, current - start);
             _tokens.Add(new Token(type, text, literal, line));
         }
     }
