6164d1ce2be545157f5e895f04e1cc49ab91f330
Matthew Herman <mattherman11@gmail.com>
2021-04-30 12:43:58 -0500

    Added closures

--- Lox/Interpreter.cs
+++ Lox/Interpreter.cs
@@ -212,7 +212,7 @@ namespace Lox
 
         public object VisitFunctionStmt(Stmt.Function stmt)
         {
-            var function = new LoxFunction(stmt);
+            var function = new LoxFunction(stmt, _environment);
             _environment.Define(stmt.Name.Lexeme, function);
             return null;
         }
--- Lox/LoxFunction.cs
+++ Lox/LoxFunction.cs
@@ -6,17 +6,19 @@ namespace Lox
     public class LoxFunction : ILoxCallable
     {
         private readonly Stmt.Function _declaration;
+        private readonly Environment _closure;
 
         public int Arity { get { return _declaration.Parameters.Count; } }
 
-        public LoxFunction(Stmt.Function declaration)
+        public LoxFunction(Stmt.Function declaration, Environment closure)
         {
             _declaration = declaration;
+            _closure = closure;
         }
 
         public object Call(Interpreter interpreter, List<object> arguments)
         {
-            var environment = new Environment(interpreter.Globals);
+            var environment = new Environment(_closure);
             foreach (var (parameter, argument) in _declaration.Parameters.Zip(arguments))
             {
                 environment.Define(parameter.Lexeme, argument);
